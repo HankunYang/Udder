2d28a2063cffc3bb2c4c8beee98ceb9a5fb4ad87
Hankun Yang <hankun.yang@outlook.com>
2019-04-15 21:24:54 +0200

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Hankun Yang on 15.04.19.
+//  Copyright © 2019 Hankun Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
